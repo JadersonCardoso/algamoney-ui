f31b4e4f609b7c3478e6b6c6f26f923dc29cb551
Jaderson <jaderson.brandao@gmail.com>
2018-04-16 12:27:15 -0300

    10.10. Criando o componente de pesquisa de lançamentos

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -3,18 +3,25 @@ import { NgModule } from '@angular/core';
 
 import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/components/table/table';
+import {TooltipModule} from 'primeng/components/tooltip/tooltip';
+
 
 import { AppComponent } from './app.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosPesquisaComponent
   ],
   imports: [
     BrowserModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    TableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
